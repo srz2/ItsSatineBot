1241456729e7cbae3dfd57f28b3a650f4bbc4dee
srz2 <srz222@gmail.com>
2019-11-19 00:29:33 -0500

    Cleaned up code and moved main commands to own function

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
-import configparser
-
 import time
+import configparser
 from datetime import datetime, timedelta
 
 import praw
 import praw.exceptions
 
-
 boo_SendReply = True
 
 # subreddits_to_monitor = 'Test'
-# subreddits_to_monitor = 'all'
+subreddits_to_monitor = 'all'
 
 bot_username = ''
 
@@ -69,50 +67,55 @@ def comment_reply_message(comment, message):
 		return new_comment
 	except praw.exceptions.APIException as e:
 		print e
-		print 'Need to sleep: ' + str(reddit.auth.limits['remaining'])
 		return None
 
-config = configparser.ConfigParser()
-config.read('bot.cfg')
+def start_bot():
+	config = configparser.ConfigParser()
+	config.read('bot.cfg')
 
-client_id = config['DEFAULT']['CLIENT_ID']
-client_secret = config['DEFAULT']['CLIENT_SECRET']
-username = config['DEFAULT']['USERNAME']
-password = config['DEFAULT']['PASSWORD']
-user_agent = config['DEFAULT']['USER_AGENT']
+	client_id = config['DEFAULT']['CLIENT_ID']
+	client_secret = config['DEFAULT']['CLIENT_SECRET']
+	username = config['DEFAULT']['USERNAME']
+	password = config['DEFAULT']['PASSWORD']
+	user_agent = config['DEFAULT']['USER_AGENT']
 
-reddit = praw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     password=password,
-                     user_agent=user_agent,
-                     username=username)
+	reddit = praw.Reddit(client_id=client_id,
+	                     client_secret=client_secret,
+	                     password=password,
+	                     user_agent=user_agent,
+	                     username=username)
 
-bot_username = str(reddit.user.me())
-print('Running bot....' + bot_username)
+	bot_username = str(reddit.user.me())
+	print('Running bot....' + bot_username)
 
-subreddit = reddit.subreddit(subreddits_to_monitor)							# subscribe to /r/all
-for comment in subreddit.stream.comments():									# Stream all comments
-	if satan_exists(comment.body):									# Check if 'satan' exists in comment body
-		if not bot_exists_in_parent_comments(comment):						# Check if this bot exists in any parent comments
-			if not bot_exists_in_sibling_comments(comment):					# Check to make sure comment hasn't already been made
-				# show_comment(comment)										# [DEBUG] Show comment
-				if boo_SendReply:
-					msg = create_satine_reply_message()
-					if comment_reply_message(comment, msg) == None:			# Attempt to send message
-						seconds = reddit.auth.limits['remaining']			# If it fails, wait for x amount of seconds
-						now = datetime.now()
-						expire = now + timedelta(seconds=seconds)
-						print str(now.time()) + ' - Reddit Sending timeout. Sleeping for ' + str(seconds) + ' seconds. Sending at ' + str(expire.time())
-						time.sleep(seconds)
-						if comment_reply_message(comment, msg) == None:
-							print 'Failed to send reply'
-# 		# 		else:
-# 		# 			print 'Sending replies is turned off'
-# 			else:
-# 				print 'Bot reply exists in sibling comment'
-# 		else:
-# 			print 'Bot reply exists in a parent comment'
-# 	else:
-# 		print 'A non-satan comment is here'
-# 		show_comment(comment)
-# 		Dont print anything here, otherwise all comments will be printed
+	subreddit = reddit.subreddit(subreddits_to_monitor)							# subscribe to /r/all
+	for comment in subreddit.stream.comments():									# Stream all comments
+		if satan_exists(comment.body):											# Check if 'satan' exists in comment body
+			if not bot_exists_in_parent_comments(comment):						# Check if this bot exists in any parent comments
+				if not bot_exists_in_sibling_comments(comment):					# Check to make sure comment hasn't already been made
+					# show_comment(comment)										# [DEBUG] Show comment
+					if boo_SendReply:
+						msg = create_satine_reply_message()
+						if comment_reply_message(comment, msg) == None:			# Attempt to send message
+							seconds = reddit.auth.limits['remaining']			# If it fails, wait for x amount of seconds
+							print 'Need to sleep: ' + str(reddit.auth.limits['remaining'])  + ' seconds'
+							now = datetime.now()
+							expire = now + timedelta(seconds=seconds)
+							print str(now.time()) + ' - Reddit Sending timeout. Sleeping for ' + str(seconds) + ' seconds. Sending at ' + str(expire.time())
+							time.sleep(seconds)
+							if comment_reply_message(comment, msg) == None:
+								print 'Failed to send reply'
+	# 		# 		else:
+	# 		# 			print 'Sending replies is turned off'
+	# 			else:
+	# 				print 'Bot reply exists in sibling comment'
+	# 		else:
+	# 			print 'Bot reply exists in a parent comment'
+	# 	else:
+	# 		print 'A non-satan comment is here'
+	# 		show_comment(comment)
+	# 		Dont print anything here, otherwise all comments will be printed
+
+
+if __name__ == '__main__':
+	start_bot();
